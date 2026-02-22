9e3d9e6430d1f82369c5854dd747bfe194328176
chumingi <chumingi794@gmail.com>
2026-02-22 14:31:06 +0900

    section 02 - 2026-02-21

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -22,3 +22,31 @@ function fruitProcessor(apples, oranges) {
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 console.log(fruitProcessor(1, 3));
+
+// Function declaration vs expressions
+function calcAge(birthYear) {
+    return 2026 - birthYear;
+}
+const calcAge2 = function (birthYear) {
+    return 2026 - birthYear;
+}
+const age1 = calcAge(2004);
+const age2 = calcAge2(2004);
+console.log(age1, age2);
+
+// arrow function (화살표 함수)
+const calcAge3 = birthYear => 2026 - birthYear;
+const age3 = calcAge3(2004);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2026 - birthYear;
+    const retirement = 65 - age;
+    if (retirement <= 0) {
+        return `${firstName} has already retired.`;
+    } else {
+        return `${firstName} retires in ${retirement} years`;
+    }
+}
+console.log(yearsUntilRetirement(2004, "Mingi"));
+console.log(yearsUntilRetirement(1950, "Mark"));
