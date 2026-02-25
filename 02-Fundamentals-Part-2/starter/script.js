519a517e47fd3dd1aafd6ffa05e9a7b1a6254a3e
chumingi <chumingi794@gmail.com>
2026-02-25 12:32:40 +0900

    section 02 - 2026-02-22

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -50,3 +50,46 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 console.log(yearsUntilRetirement(2004, "Mingi"));
 console.log(yearsUntilRetirement(1950, "Mark"));
+
+// Array 생성
+const friends = ['Michle', 'Steven', 'Peter'];
+console.log(friends);
+const years = new Array(1991, 2002, 2026);
+console.log(years);
+
+// Array 항목에 접근 - 인덱스는 0부터 시작
+console.log(friends[0]);  // MIchle
+console.log(friends.length);  // 3
+console.log(friends[friends.length - 1]);  // Peter
+
+// Array의 항목(element) 변경
+friends[2] = "jay";  // 요소 변경
+console.log(friends);
+
+// 다양한 타입 저장 가능
+const firstName = "Mingi";
+const mingi = [firstName, 2004, "Senior in university", friends];
+console.log(mingi);
+// const calcAge = (birthYear) => 2026 - birthYear;
+const birthYears = [1991, 2004, 2020];
+const ages = [
+    calcAge(birthYears[0]),
+    calcAge(birthYears[1]),
+    calcAge(birthYears[2])
+];
+console.log(ages);
+
+// Array 메소드 (method)
+friends.push("Peter");  // Array의 맨 끝에 요소 추가
+console.log(friends);  // ["Michle", "Steven", "Jay", "Peter"]
+friends.unshift("John");  // Array의 맨 앞에 요소 추가
+console.log(friends);  // ["John", "Michle", "Steven", "Jay", "Peter"]
+const popped = friends.pop();  // Array의 마지막 요소 제거
+console.log(popped, friends);  // Peter ["Jogn", "Michle", "Steven", "Jay"]
+friends.shift();  // Array의 맨 앞 요소 제거
+console.log(friends);  // ["Michle", "Steven", "Jay"]
+console.log(friends.indexOf("Steven"));  // 1, Array에서 해당 요소의 위치 찾기
+console.log(friends.indexOf("Bob"));  // 존재하지 않는 요소는 -1 반환
+console.log(friends.includes("Steven"));  // true, Array 내에서 해당 요소의 존재 여부 반환
+console.log(friends.includes("Bob"));  // false
+console.log(birthYears.includes("2004"));  // false, 타입까지도 일치해야 true (strict)
