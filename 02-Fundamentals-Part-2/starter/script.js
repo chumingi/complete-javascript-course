cf9038fa223acf33d8b577996398c61f245fcc5f
chumingi <chumingi794@gmail.com>
2026-02-25 23:57:35 +0900

    section 02 - 2026-02-25

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -93,3 +93,65 @@ console.log(friends.indexOf("Bob"));  // 존재하지 않는 요소는 -1 반환
 console.log(friends.includes("Steven"));  // true, Array 내에서 해당 요소의 존재 여부 반환
 console.log(friends.includes("Bob"));  // false
 console.log(birthYears.includes("2004"));  // false, 타입까지도 일치해야 true (strict)
+
+
+// Object
+const chumingi = {
+    firstName: "Mingi",
+    lastName: "Chu",
+    age: 2026 - 2004,
+    job: 'Senior in university',
+    friends: friends,
+};
+console.log(chumingi);
+
+// Object의 프로퍼티에 접근하는 방법 (Dot vs Bracket Notation)
+    // Dot
+console.log(chumingi.firstName);  // Mingi
+// console.log(chumingi."first"+"Name";  // Uncaught SyntaxError: Unexpected string
+    // Bracket
+const nameKey = "Name";
+console.log(chumingi["first" + nameKey]);  // Mingi
+console.log(chumingi["last" + nameKey]);  // Chu
+    // Dot + Bracket 예시
+console.log(`${chumingi.firstName} has ${chumingi.friends.length} friends, and his best friends is called ${chumingi.friends[0]}`);
+    // 사용자 입력 예시
+const interestedIn = prompt("What do you want know about Mingi? Choose between firstName, lastName, age, job and friends.");
+console.log(chumingi.interestedIn);  // undefined
+console.log(chumingi[interestedIn]);  // prompt()로 입력받은 키에 해당하는 값 반환
+    // Object에 새 속성(property) 추가
+chumingi.country = "Korea";
+chumingi["university"] = "Gachon";
+console.log(chumingi);
+
+// Object Method
+const methodChumingi = {
+    firstName: "Mingi",
+    lastName: "Chu",
+    birthYear: 2004,
+    job: 'Senior in university',
+    friends: friends,
+    hasDriversLicence: false,
+
+    calcAge: function () {
+        return 2026 - this.birthYear;
+    },
+    calcAgeStore: function () {
+        this.age = 2026 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicence ? 'a' : 'no'} driver's licence.`;
+    },
+};
+console.log(methodChumingi.calcAge());
+console.log(methodChumingi.calcAge());
+console.log(methodChumingi['calcAge']());
+console.log(methodChumingi.getSummary());
+
+// calcAGe() 호출 후에는 age를 바로 호출 가능
+console.log(methodChumingi.calcAgeStore());
+console.log(methodChumingi.age);
+console.log(methodChumingi.age);
+console.log(methodChumingi.age);
+console.log(methodChumingi.age);
