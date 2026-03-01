e1d1466bc6e6b56937119d27708a231c8c065f09
chumingi <chumingi794@gmail.com>
2026-03-01 16:41:55 +0900

    section 03 - 2026-03-01

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -6,3 +6,40 @@ if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2026 - birthYear;
 console.log(calcAge(2004));
+
+// Debugging
+const mesureKelvin = function () {
+  const messurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius')),
+  };
+
+  console.log(messurement);
+  console.table(messurement);
+
+  const kelvin = messurement.value + 273;
+  return kelvin;
+};
+console.log(mesureKelvin());
+
+// use debugger
+function calcTempAmplitudeBug(t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger; // 브라우저에서 디버깅을 시작할 위치에 이 줄을 추가
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+    console.log(max, min);
+  }
+  return max - min;
+}
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
