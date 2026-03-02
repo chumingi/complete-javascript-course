c256318da4d6b9738d7b336e39435502dd2060be
chumingi <chumingi794@gmail.com>
2026-03-02 18:36:52 +0900

    section 05 - Implement the highscore functionality)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,6 +13,7 @@ let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let questionMark = '?';
 document.querySelector('.number').textContent = questionMark;
+let highScore = 0;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
@@ -24,6 +25,11 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('body').style.backdropFilter = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too High';
