0f3bbdec33eddea51e0eab53c5e34907a8d7b213
chumingi <chumingi794@gmail.com>
2026-03-02 17:43:08 +0900

    section 05 - coding challenge (Implement the game reset functionality)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,8 +10,9 @@
 // console.log(document.querySelector('.guess').value);
 
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let questionMark = '?';
+document.querySelector('.number').textContent = questionMark;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
@@ -22,6 +23,7 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = 'Correct Number';
     document.querySelector('body').style.backdropFilter = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too High';
@@ -39,4 +41,18 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = 'You lost the game';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  console.log('Reset game');
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = questionMark;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing';
+
+  document.querySelector('body').style.backdropFilter = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
