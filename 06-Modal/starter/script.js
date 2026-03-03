abe5125b9117ba441d23137f38b65fb65fc791ef
chumingi <chumingi794@gmail.com>
2026-03-03 12:17:17 +0900

    section 06 - working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,7 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
+const openModal = btnText => {
+  modal.classList.remove('hidden'); // modal 요소의 hidden 클래스 제거
+  overlay.classList.remove('hidden'); // .은 선택자이기 때문에 제외하고 클래스명만 작성
+  console.log(`\"${btnText}\" button is opened`);
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden'); // modal 요소에 hidden 클래스 추가
+  overlay.classList.add('hidden');
+  console.log('modal is closed');
+};
+
 console.log(btnsShowModal);
 for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+  let btn = btnsShowModal[i];
+  btn.addEventListener('click', () => openModal(btn.textContent));
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
