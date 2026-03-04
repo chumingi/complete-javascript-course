ef48e41b3faf2b677883a22eb9502ced69267313
chumingi <chumingi794@gmail.com>
2026-03-04 21:40:54 +0900

    section 07 - Rolling the dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,7 +4,34 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+const current0El = document.querySelector('#current--0');
+const current1El = document.getElementById('current--1');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', () => {
+  /*
+  - 랜덤 주사위 숫자 생성
+  - 주사위 숫자 보여주기
+  - 주사위 수자가 1인지 확인하기
+  - 참이면 다음 플레이어로 넘어가기
+  */
+  const dice = Math.trunc(Math.random() * 6);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  console.log(`Rolled the dice (${dice})`);
+
+  // 주사위 숫자가 1인지 확인
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
