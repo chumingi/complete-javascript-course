4c9b0461ae7bce0886dddddb773cf0e94e390a4d
chumingi <chumingi794@gmail.com>
2026-03-05 16:54:37 +0900

    section 07 - Holding current score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -16,8 +16,18 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0; // 0: Player0, 1: Player1
+let playing = true;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player-active');
+  player1El.classList.toggle('player-active');
+};
 
 btnRoll.addEventListener('click', () => {
   /*
@@ -26,21 +36,47 @@ btnRoll.addEventListener('click', () => {
   - 주사위 수자가 1인지 확인하기
   - 참이면 다음 플레이어로 넘어가기
   */
-  const dice = Math.trunc(Math.random() * 6);
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  console.log(`Rolled the dice (${dice})`);
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    console.log(`Rolled the dice (${dice})`);
 
-  // 주사위 숫자가 1인지 확인
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player-active');
-    player1El.classList.toggle('player-active');
+    // 주사위 숫자가 1인지 확인
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', () => {
+  /*
+    - 진행 중인 플ㄹ에이어의 현재 점수를 총점수에 더하기
+    - 진행 중인 플레이어의 총점수가 100보다 큰지 확인
+      -- 참이면 진행 중인 플레이어 승
+      - 거짓이면 다음 플레이어로 넘어가기
+  */
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      console.log(`Player${activePlayer} wons the game!`);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
   }
 });
