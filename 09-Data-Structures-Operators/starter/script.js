8550dfe603ea7bc6e83e588ed9e04a158d1ba29e
chumingi <chumingi794@gmail.com>
2026-03-26 22:22:47 +0900

    section 09 - 2026-03-26

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -245,3 +245,34 @@ restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
 restaurant.guestCorrect = 0;
 const guestCorrect = restaurant.guestCorrect ?? 10;
 console.log(guestCorrect); // 0
+
+// Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+const rest2 = {
+  name: 'La Pizza',
+  owner: 'Chu Mingi',
+};
+
+// OR assignment operator (??=)
+// rest1.numGuests = rest1.numGuests || 10;
+rest1.numGuests ||= 10;
+// rest2.numGuests = rest2.numGuests || 10;
+rest2.numGuests ||= 10;
+console.log(rest1.numGuests, rest2.numGuests); // 20 10
+
+// NULLISH assignment operator (??=)
+rest1.numGuests = 0;
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+console.log(rest1.numGuests, rest2.numGuests); // 0 10
+
+// AND assignment operator (??=)
+// rest1.owner = rest1.owner && '<ANOYMOUS>';
+rest1.owner &&= '<ANOYMOUS>';
+// rest2.owner = rest2.owner && '<ANOYMOUS>';
+rest2.owner &&= '<ANOYMOUS>';
+console.log(rest1.owner, rest2.owner); // undefined <ANOYMOUS>
